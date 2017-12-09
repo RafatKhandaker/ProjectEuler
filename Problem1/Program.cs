dbd2de2491b8b378ebd29616041e01c4f7ec3bff
Rafael Adam Soto <rooksoto@gmail.com>
2017-12-09 07:52:06 -0500

    Initial commit

---
+++ Problem1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectEuler.Problem1
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
